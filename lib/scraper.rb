6c1078a3f35f52bdc61e7b474248cbe1bf014bd1
Stela Capsa <stela.capsa@gmail.com>
2020-01-13 03:54:26 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+puts doc.css(".headline-26OIBN").text 
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
